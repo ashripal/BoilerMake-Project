e784364d788c304ed206dcb79f72ca328113f24b
Neha Sharma <nehasharma.c2021@gmail.com>
2023-01-21 11:07:45 -0500

    Modified nearest_color_name function

--- color_detection.py
+++ color_detection.py
@@ -1,24 +1,22 @@
+import numpy
+import matplotlib.image as mp_img
 import cv2
 from sklearn.cluster import KMeans
+import math
 
 def nearest_color_name(center):
     colors = {
         "red": (255,0,0),
         "green": (0,255,0),
         "blue": (0,0,255),
-        "yellow": (255,255,0),
-        "black": (0,0,0),
-        "white": (255,255,255),
-        "grey": (128,128,128),
-        "purple": (128,0,128),
-        "pink": (255,105,180),
-        "orange": (255,140,0),
-        "brown": (165,42,42)
+        "yellow": (0,255,255)
     }
     dist_min = float("inf")
+    color = None
+
     for color, value in colors.items():
-        distance = sum([(c - v) ** 2 for c,v in zip(center, value)])
-        if distance < dist_min:
+        distance = math.dist(center, value)
+        if (distance < dist_min):
             dist_min = distance
             nearest_color = color
     print(nearest_color)
@@ -26,8 +24,10 @@ def nearest_color_name(center):
 
 def color_detection(image_file):
     image = cv2.cvtColor(cv2.imread(image_file), cv2.COLOR_BGR2RGB)
+    #image_file = image_file[:,:,[2,1,0]]
+    #image = mp_img.imread(image_file)
 
-    image = image.reshape((image.shape[1]*image.shape[0], 3))
+    #image = image.reshape((image.shape[1]*image.shape[0], 3))
 
     #finds most prominent color
     color_classification = KMeans(n_clusters = 1)
@@ -35,18 +35,22 @@ def color_detection(image_file):
     
     labels = color_classification.labels_
     labels = list(labels)
-    print(labels)
+    #print(labels)
 
     centroid = color_classification.cluster_centers_
-    print(tuple(centroid))
+    #print(tuple(centroid))
 
     #gets color name from rgb value
     color = nearest_color_name(tuple(centroid))
     return color
 
 def main():
-    color = nearest_color_name(((255,255,0)))
-    print(str(color))
+    color = nearest_color_name(((0,255,255)))
+    #print(str(color))
+    color_detection("apple_test.jpeg")
+    color_detection("orange_test.jpeg")
+    color_detection("green_test.jpeg")
+    color_detection('blue_test.jpeg')
 
 if __name__ == "__main__":
     main()
